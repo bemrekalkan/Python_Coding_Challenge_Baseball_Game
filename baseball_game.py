9a23e1cdb9e11e1d13bfd90deccbab0d0b92feb7
bemrekalkan <bemre.kalkan@gmail.com>
2022-08-20 15:59:38 +0300

    second solution added

--- baseball_game.py
+++ baseball_game.py
@@ -1,4 +1,7 @@
-# SOLUTION 👇
+# SOLUTION-1 👇
+
+from curses.ascii import isalnum
+
 ops = ["5", "-2", "4", "C", "D", "9", "+", "+"]
 def baseball(list):
     new_list = []
@@ -15,3 +18,31 @@ def baseball(list):
 
 
 print(baseball(ops))
+
+
+# SOLUTION-2 👇
+
+ops = ["5", "-2", "4", "C", "D", "9", "+", "+"] 
+
+def calculate_points(ops):
+    points = []
+    
+    for op in ops:
+        if op.isdigit() or "-" in op:
+            points.append(int(op))
+            print(points)
+        else:
+            if op == "C" and op != ops[0] and len(points) >= 1:
+                points.pop()
+
+            elif op == "D" and op != ops[0] and len(points) >= 1:
+                points.append(points[-1]*2)
+
+            elif op == "+" and op != ops[0] and len(points) >= 2:
+                points.append(points[-1] + points[-2])
+
+            print(points)
+    return sum(points)
+
+print(calculate_points(ops))
+
